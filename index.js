e3b922db5361f827e801d7f57429f75d7df2c649
Ben Vinnick <benvinnick@gmail.com>
2021-12-25 19:49:51 -0800

    updated app to hold port and listen

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const path = require("path");
+const app = require("./app");
 const PORT = process.env.PORT || 5000;
-
-app.use(express.json());
-app.use(cors());
-
-// serve build if in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-}
-
-// ----- ROUTES ----- //
-
-// register/login route:
-
-app.use("/auth", require("./routes/jwtAuth"));
-
-// home route:
-
-app.use("/home", require("./routes/home"));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
-  // this sends heroku to the client page and then displays the route "*". If this is some random route then it will not work.
-});
-
-// problem... heroku is using server routes instead of client routes when you type them into url
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
